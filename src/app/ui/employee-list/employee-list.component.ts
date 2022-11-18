59944da04b7e12cd2c8f266d287787410f108c02
Patrycja <czerwionkapatrycja@gmail.com>
2022-11-18 16:25:12 +0100

    feat: change according to new PersonalModel

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EmployeeService } from '../../services/employee.service';
 import { EmployeeModel } from '../../model/employee.model';
+import {PersonModel} from "../../model/person.model";
 
 @Component({
   selector: 'employee-list',
@@ -12,7 +13,7 @@ import { EmployeeModel } from '../../model/employee.model';
 })
 export class EmployeeListComponent {
   title: string = 'Lista pracowników';
-  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
+  data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
   constructor(private _employeeService: EmployeeService) { }
 
 
--- src/app/ui/employees-faces/employees-faces.component.ts
+++ src/app/ui/employees-faces/employees-faces.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import {EmployeeModel} from "../../model/employee.model";
 import {Observable} from "rxjs";
 import {EmployeeService} from "../../services/employee.service";
+import {PersonModel} from "../../model/person.model";
 
 @Component({
   selector: 'app-employees-faces',
@@ -11,7 +12,7 @@ import {EmployeeService} from "../../services/employee.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EmployeesFacesComponent {
-  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
+  data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
   constructor(private _employeeService: EmployeeService) { }
 
 }

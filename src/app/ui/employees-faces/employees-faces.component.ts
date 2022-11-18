0b74288f904235fa20b6b658993b649f620c379c
Patrycja <czerwionkapatrycja@gmail.com>
2022-11-18 14:41:51 +0100

    feat: added httpClient

---
+++ src/app/ui/employees-faces/employees-faces.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {EmployeeModel} from "../../model/employee.model";
+import {Observable} from "rxjs";
+
+@Component({
+  selector: 'app-employees-faces',
+  templateUrl: './employees-faces.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeesFacesComponent {
+  data$: Observable<EmployeeModel[]> = this._httpClient.get<EmployeeModel[]>('assets/data/employees.json')
+  constructor(private _httpClient: HttpClient) {}
+}

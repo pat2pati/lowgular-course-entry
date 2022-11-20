55d2c2cc63b877d4c03c492071785db34ae3d27f
Patrycja <czerwionkapatrycja@gmail.com>
2022-11-20 19:48:31 +0100

    feat: new Api map

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -4,23 +4,50 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PersonModel } from '../model/person.model';
 import { CreateEmployeeModel } from '../model/create-employee.model';
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from './employee.response';
+
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
 
+
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('assets/data/people.json');
-  }
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>
+    ('https://dummy.restapiexample.com/api/v1/employees',
+    ).pipe(
+      map( (response: ApiResponse<EmployeeResponse[]>) =>{
+        return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+            name: employeeResponse.employee_name,
+            personalNumber: employeeResponse.id,
+            mail: employeeResponse.employee_name + '@lowgular.com',
+            img:employeeResponse.profile_image,
+            salary:employeeResponse.employee_salary,
+            age: employeeResponse.employee_age,
+          }
+          })
+        }))
+      }
+
+
+
+
+  // getAll(): Observable<PersonModel[]> {
+  //   return this._httpClient.get<PersonModel[]>('assets/data/people.json');
+  // }
+
 
   create(employees: CreateEmployeeModel): Observable<void> {
     return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employees).pipe(map(_ => void 0));
   }
 
-  delete(id: string): Observable<void> {
+  delete(id: number): Observable<void> {
     return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete/' + id).pipe(map(_ => void 0));
   }
+
 }
 
 

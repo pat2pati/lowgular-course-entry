c8692d6f6ab7ea1be40a352068de45d9966baac2
Patrycja <czerwionkapatrycja@gmail.com>
2022-11-20 19:46:50 +0100

    feat: created employee-response

---
+++ src/app/services/employee.response.ts
@@ -0,0 +1,8 @@
+export interface EmployeeResponse {
+  readonly id: number;
+  readonly employee_name: string;
+  readonly employee_salary: string;
+  readonly employee_age: number;
+  readonly profile_image: string;
+
+}

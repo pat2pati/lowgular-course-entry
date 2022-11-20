36450575bc8dd78b4194787b8f9f90db1f3fc046
Patrycja <czerwionkapatrycja@gmail.com>
2022-11-20 14:19:33 +0100

    feat: change api

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 
 import {Observable} from "rxjs";
 import {PersonModel} from "../model/person.model";
+import {CreateEmployeeModel} from "../model/create-employee.model";
 
 @Injectable()
 export class EmployeeService {
@@ -12,4 +13,7 @@ export class EmployeeService {
    return this._httpClient.get<PersonModel[]>('assets/data/people.json');
   }
 
+  create(employees: CreateEmployeeModel): Observable<any>{
+    return this._httpClient.post('https://jsonplaceholder.typicode.com/posts', employees);
+  }
 }
